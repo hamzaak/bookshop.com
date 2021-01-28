8768af6c6bd1437350149d8b33951122b0f77a29
Hamza Ak <ak.hamza.88@gmail.com>
2021-01-28 15:37:20 +0300

    cart fix

--- ui/src/api/cartClient.js
+++ ui/src/api/cartClient.js
@@ -3,7 +3,7 @@ import { API_CART_URL } from "@/api/config";
 
 const cartClient = {
     get(accountId) {
-      return axios.get(API_CART_URL + `/get` + accountId).then(res => res.data);
+      return axios.get(API_CART_URL + `/get/` + accountId).then(res => res.data);
     },
     add(cartItem) {
       return axios.post(API_CART_URL + `/add`, cartItem).then(res => res.data);
